7dade688e42419f9983eda8ad75aebcce1da9432
Vaughn Walters <vaughnwalters@gmail.com>
2016-06-20 17:50:23 -0500

    added validation to keyup event

--- events.js
+++ events.js
@@ -2,26 +2,34 @@ var CarLot = (function (events) {
   var targetDiv;
   var cards = document.getElementsByClassName("newDiv");
   var input = document.getElementById("inputBox");
-    events.activateEvents = function() {
-      for (var i = 0; i < cards.length; i++) {
-        var card = cards[i];
+  events.activateEvents = function() {
+    for (var i = 0; i < cards.length; i++) {
+      var card = cards[i];
 
 // created card click event. call function. pass element clicked on
 // and a color. function will expand border size and change background color,
 // on click of the car element, clear the value of the text 
 // input in the navbar, and put the cursor in the text input.
-        card.addEventListener("click", function (clickEvent) {
-          targetDiv = clickEvent.currentTarget;
-          events.resetValues(targetDiv);
-          events.increaseBorder(targetDiv, "gray");
-          events.clearInput();
-        });
-      };   
+      card.addEventListener("click", function (clickEvent) {
+        targetDiv = clickEvent.currentTarget;
+        // console.log("targetDiv", targetDiv);
+        events.resetValues(targetDiv);
+        events.increaseBorder(targetDiv, "gray");
+        events.clearInput();
+      });
+    };   
 
 // add Event listener for keyup
-      input.addEventListener("keyup", function (keyupEvent){
+    input.addEventListener("keyup", function (keyupEvent){
+
+// if no card has been selected when user types in input field, throw an alert, 
+// else change the description to what is being typed.
+      if (targetDiv === undefined) {
+        alert ("Please select a card");
+      } else {
         events.changeDescription(targetDiv, keyupEvent);
-      });
-    };
+      };
+    });
+  };
   return events;  
 })(CarLot);
